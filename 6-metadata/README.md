b9ded38bb0507b2e935af1f6f050236b62e88c20
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-10-26 13:33:08 +0330

    metadata

---
+++ 6-metadata/README.md
@@ -0,0 +1,1 @@
+# React19 + metadata
---
+++ 6-metadata/src/App.tsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+  return (
+    <>
+    <title>react 19</title>
+    <meta name="author" content="react 19" />
+    </>
+  )
+}
+
+export default App
